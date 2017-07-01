07b779a1fc12eff156b3bf3856f970dae4be03ff
Mohsin Falak <fi8er1@gmail.com>
2017-07-02 01:06:36 +0500

    2nd Commit

---
+++ app/src/main/java/com/byteshaft/cellexchange/MainActivity.java
@@ -0,0 +1,28 @@
+package com.byteshaft.cellexchange;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+import com.byteshaft.cellexchange.fragments.WelcomeFragment;
+import com.byteshaft.cellexchange.utils.Helpers;
+
+public class MainActivity extends FragmentActivity {
+
+    public static FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        Helpers.loadFragment(fragmentManager, new WelcomeFragment(), "WelcomeFragment");
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
